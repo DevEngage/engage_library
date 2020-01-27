d4326230df214d35e59c0aa6a84819ef2e18db18
James Brinkerhoff <th3brink@gmail.com>
2020-01-26 21:16:29 -0700

    added some default stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:earn_it/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -45,6 +46,12 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+   HomeScreen(),
+   Text('test 2'),
+   Text('test 3'),
+ ];
 
   void _incrementCounter() {
     setState(() {
@@ -57,6 +64,12 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -66,46 +79,28 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+     appBar: AppBar(
+       title: Text('Games Revealed'),
+     ),
+     body: _children[_currentIndex], // new
+     bottomNavigationBar: BottomNavigationBar(
+       onTap: onTabTapped, // new
+       currentIndex: _currentIndex, // new
+       items: [
+         new BottomNavigationBarItem(
+           icon: Icon(Icons.home),
+           title: Text('Home'),
+         ),
+         new BottomNavigationBarItem(
+           icon: Icon(Icons.mail),
+           title: Text('Following'),
+         ),
+         new BottomNavigationBarItem(
+           icon: Icon(Icons.person),
+           title: Text('Profile')
+         )
+       ],
+     ),
+   );
   }
 }
---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class ProfileScreen extends HookWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('test'); // ListView.builder()
+  }
+
+}
