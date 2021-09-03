837eca0a164df1f96ee0d4a39e2ca5cddf65738b
James Brinkerhoff <th3brink@gmail.com>
2021-09-02 23:03:10 -0600

    working on init

--- lib/engage.dart
+++ lib/engage.dart
@@ -1,3 +1,20 @@
 export './constants/constants.dart';
 export './screens/screens.dart';
 export './widgets/widgets.dart';
+
+class Engage {
+  static bool enableAds = true;
+  static bool enableAnalytics = true;
+  static bool enableFirestore = true;
+  static bool enableAutoLoginAnon = true;
+  Engage();
+
+  static init({
+    enableAds = true,
+    enableAnalytics = true,
+    enableFirestore = true,
+    enableAutoLoginAnon = true,
+  }) async {
+    
+  }
+}
